91936a311aa22409bf192f0f79ef122dd61b6592
Dave Derderian <dave@drt.sh>
2019-07-08 15:46:16 +0900

    separate out PopulateDatabase

--- db/db.go
+++ db/db.go
@@ -41,78 +41,138 @@ func Connect() {
 
 func PopulateDatabase(entries []*SGEntry) (err error) {
 	fmt.Println("[INFO] Populating Database")
-	// drop and create the virtual table
-	fmt.Println("[DEBUG] DROP tables")
-	if _, err = SQL.Exec("DROP TABLE IF EXISTS einihongo"); err != nil {
+
+	err = dropTables()
+	if err != nil {
 		return
 	}
-	if _, err := SQL.Exec("DROP TABLE IF EXISTS entity_members"); err != nil {
+
+	err = createTables()
+	if err != nil {
+		return
+	}
+
+	err = insertEntities()
+	if err != nil {
+		return
+	}
+
+	return insertData(entries)
+}
+
+func dropTables() error {
+	// drop and create the virtual table
+	fmt.Println("[DEBUG] DROP tables")
+	tx, err := SQL.Begin()
+	if err != nil {
 		return err
 	}
-	if _, err := SQL.Exec("DROP TABLE IF EXISTS definitions"); err != nil {
+
+	if _, err = tx.Exec("DROP TABLE IF EXISTS einihongo"); err != nil {
+		tx.Rollback()
 		return err
 	}
-	if _, err := SQL.Exec("DROP TABLE IF EXISTS readings"); err != nil {
+	if _, err := tx.Exec("DROP TABLE IF EXISTS entity_members"); err != nil {
+		tx.Rollback()
 		return err
 	}
-	if _, err := SQL.Exec("DROP TABLE IF EXISTS sense_misc"); err != nil {
+	if _, err := tx.Exec("DROP TABLE IF EXISTS definitions"); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err := tx.Exec("DROP TABLE IF EXISTS readings"); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err := tx.Exec("DROP TABLE IF EXISTS sense_misc"); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	tx.Commit()
+	return nil
+}
+
+func createTables() error {
+	tx, err := SQL.Begin()
+	if err != nil {
 		return err
 	}
 
 	// create all tables
 	fmt.Println("[DEBUG] CREATE tables")
-	if _, err = SQL.Exec("CREATE VIRTUAL TABLE einihongo USING fts4(entryid,japanese,furigana,english,romaji,freq)"); err != nil {
-		return
-	}
-	if _, err := SQL.Exec("CREATE TABLE entity_members(abbvr TEXT PRIMARY KEY, meaning TEXT)"); err != nil {
+	if _, err = tx.Exec("CREATE VIRTUAL TABLE einihongo USING fts4(entryid,japanese,furigana,english,romaji,freq)"); err != nil {
+		tx.Rollback()
 		return err
 	}
-	if _, err := SQL.Exec("CREATE TABLE definitions(id INTEGER PRIMARY KEY AUTOINCREMENT, entryid INTEGER, pos TEXT, gloss TEXT, FOREIGN KEY(entryid) REFERENCES einihongo(entryid), FOREIGN KEY(pos) REFERENCES entity_members(abbvr))"); err != nil {
+	if _, err := tx.Exec("CREATE TABLE entity_members(abbvr TEXT PRIMARY KEY, meaning TEXT)"); err != nil {
+		tx.Rollback()
 		return err
 	}
-	if _, err := SQL.Exec("CREATE TABLE readings(entryid INTEGER PRIMARY KEY, japanese TEXT, furigana TEXT, altkanji TEXT, altkana TEXT, romaji TEXT, FOREIGN KEY(entryid) REFERENCES einihongo(entryid))"); err != nil {
+	if _, err := tx.Exec("CREATE TABLE definitions(id INTEGER PRIMARY KEY AUTOINCREMENT, entryid INTEGER, pos TEXT, gloss TEXT, FOREIGN KEY(entryid) REFERENCES einihongo(entryid), FOREIGN KEY(pos) REFERENCES entity_members(abbvr))"); err != nil {
+		tx.Rollback()
 		return err
 	}
-	if _, err := SQL.Exec("CREATE TABLE sense_misc(senseid INTEGER, entryid INTEGER, misc TEXT, PRIMARY KEY (senseid, entryid, misc), FOREIGN KEY(entryid) REFERENCES einihongo(entryid), FOREIGN KEY(senseid) REFERENCES definitions(id), FOREIGN KEY(misc) REFERENCES entity_members(abbvr))"); err != nil {
+	if _, err := tx.Exec("CREATE TABLE readings(entryid INTEGER PRIMARY KEY, japanese TEXT, furigana TEXT, altkanji TEXT, altkana TEXT, romaji TEXT, FOREIGN KEY(entryid) REFERENCES einihongo(entryid))"); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err := tx.Exec("CREATE TABLE sense_misc(senseid INTEGER, entryid INTEGER, misc TEXT, PRIMARY KEY (senseid, entryid, misc), FOREIGN KEY(entryid) REFERENCES einihongo(entryid), FOREIGN KEY(senseid) REFERENCES definitions(id), FOREIGN KEY(misc) REFERENCES entity_members(abbvr))"); err != nil {
+		tx.Rollback()
 		return err
 	}
 
-	// begin transaction
+	tx.Commit()
+	return nil
+}
+
+func insertEntities() error {
+	fmt.Println("[DEBUG] INSERT XmlEntities")
+
 	tx, err := SQL.Begin()
 	if err != nil {
 		return err
 	}
-	fmt.Println("[DEBUG] INSERT XmlEntities")
+
 	for abbvr, meaning := range jmdict.XmlEntities {
 		if _, err := tx.Exec("INSERT INTO entity_members(abbvr, meaning) VALUES(?,?)", abbvr, meaning); err != nil {
 			tx.Rollback()
 			return err
 		}
 	}
-	tx.Commit()
 
-	tx, err = SQL.Begin()
+	tx.Commit()
+	return nil
+}
+
+func insertData(entries []*SGEntry) error {
+	tx, err := SQL.Begin()
 	if err != nil {
 		return err
 	}
+
 	// prepare statements for tables
 	ftsStmt, err := tx.Prepare("INSERT INTO einihongo(entryid,japanese,furigana,english,romaji,freq) VALUES(?,?,?,?,?,?)")
 	if err != nil {
+		tx.Rollback()
 		return err
 	}
 
 	definitionStmt, err := tx.Prepare("INSERT INTO definitions(entryid,pos,gloss) VALUES(?,?,?)")
 	if err != nil {
+		tx.Rollback()
 		return err
 	}
 
 	readingStmt, err := tx.Prepare("INSERT INTO readings(entryid,japanese,furigana,altkanji,altkana,romaji) VALUES(?,?,?,?,?,?)")
 	if err != nil {
+		tx.Rollback()
 		return err
 	}
 
 	miscStmt, err := tx.Prepare("INSERT INTO sense_misc(senseid, entryid, misc) VALUES(?,?,?)")
 	if err != nil {
+		tx.Rollback()
 		return err
 	}
 
@@ -157,11 +217,21 @@ func PopulateDatabase(entries []*SGEntry) (err error) {
 			return err
 		}
 	}
+
+	if _, err := tx.Exec("CREATE INDEX idx_definitions_entryid ON definitions(entryid)"); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if _, err := tx.Exec("DROP VIEW IF EXISTS dirty_talk"); err != nil {
+		tx.Rollback()
+		return err
+	}
+	if _, err := tx.Exec("CREATE VIEW dirty_talk AS SELECT DISTINCT entryid FROM sense_misc WHERE misc = 'vulg' OR misc = 'sl' OR misc = 'm-sl'"); err != nil {
+		tx.Rollback()
+		return err
+	}
+
 	tx.Commit()
-
-	SQL.Exec("CREATE INDEX idx_definitions_entryid ON definitions(entryid)")
-
-	SQL.Exec("DROP VIEW IF EXISTS dirty_talk")
-	SQL.Exec("CREATE VIEW dirty_talk AS SELECT DISTINCT entryid FROM sense_misc WHERE misc = 'vulg' OR misc = 'sl' OR misc = 'm-sl'")
 	return nil
 }
